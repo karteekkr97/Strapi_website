809b9053182e1ec9d8e8fc9dff730216870db919
karteekkr97 <karteekkrshetty97@gmail.com>
2025-03-20 18:16:58 +0530

    strapi project

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', '+3iYzkGgy6qwZKVVgkDiuQ=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'gzbEzM31i2+Iuwadif4shA=='),
   },
   transfer: {
     token: {
--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['ca838791daa284dff1f75b0535dd5e6b610ce0616bae3b48ecd30c5aa1739ea7', 'y067bf506e9ea50bd4adbc03a4fad265f3b62f13a55bd372009f99da5eca29bd4']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
